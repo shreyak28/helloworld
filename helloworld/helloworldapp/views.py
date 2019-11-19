0e96842234fa0b5551cbff82b2f9cecf23f9dd09
shreya <shreyakomirelly28@gmail.com>
2019-11-19 17:36:54 +0530

    views.py changed

--- helloworld/helloworldapp/views.py
+++ helloworld/helloworldapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello (request):
-	return HttpResponse("hello welcome to the world of django")
+	return HttpResponse("hello welcome to the world of django framework")
